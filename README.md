4d6f0fba92d781e9d3abb409b259ff02219189e1
Cassandra Wilcox <csndra0504@gmail.com>
2017-02-18 20:27:44 -0500

    boostrapping horizon app

---
+++ README.md
@@ -0,0 +1,4 @@
+#hz_example_app
+
+Based on the `example_app` from [Getting Started with Horizon](http://horizon.io/docs/getting-started/)
+
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+var horizon = Horizon();
+horizon.onReady(function() {
+  document.querySelector('h1').innerHTML = 'App works!'
+});
+horizon.connect();
